5b7b134cf603633f922aaccd67d4887c4d16ae29
Evgeny Zakharov <risenforces@gmail.com>
2022-05-02 01:15:59 +0300

    refactor: types

--- src/plugins/bundled/scroll-lock.ts
+++ src/plugins/bundled/scroll-lock.ts
@@ -5,9 +5,7 @@ import {
 } from 'body-scroll-lock'
 import { createPlugin } from '../create-plugin'
 
-export type ScrollLockOptions = BodyScrollOptions
-
-export const ScrollLockPlugin = createPlugin<ScrollLockOptions | void>({
+export const ScrollLockPlugin = createPlugin<BodyScrollOptions | void>({
   build: (options) => ({
     onAfterMount: ({ screen }) => disableBodyScroll(screen, options),
     onBeforeUnmount: ({ screen }) => enableBodyScroll(screen),
