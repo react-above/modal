972913bc5a70800f5f6d3437428d5d85726e6692
Evgeny Zakharov <risenforces@gmail.com>
2022-05-06 13:57:39 +0300

    chore: update demo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import eslintPlugin from 'vite-plugin-eslint';
 
 const config = defineConfig({
+  build: {
+    outDir: 'demo-dist'
+  },
   plugins: [tsconfigPaths(), eslintPlugin()],
   server: {
     host: true,
