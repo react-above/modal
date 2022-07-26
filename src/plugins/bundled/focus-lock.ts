72a031ea4a1de1ae597630d3b44cab044834727c
Evgeny Zakharov <risenforces@gmail.com>
2022-07-26 13:41:19 +0300

    fix: better focus lock

--- src/plugins/bundled/focus-lock.ts
+++ src/plugins/bundled/focus-lock.ts
@@ -1,9 +1,19 @@
-import focusLock from 'dom-focus-lock'
+import { createFocusTrap, FocusTrap } from 'focus-trap'
 import { createPlugin } from '../create-plugin'
+
+const focusTraps = new WeakMap<HTMLDivElement, FocusTrap>()
 
 export const FocusLockPlugin = createPlugin({
   build: () => ({
-    onAfterMountDOM: ({ modal }) => focusLock.on(modal),
-    onBeforeUnmountDOM: ({ modal }) => focusLock.off(modal),
+    onAfterMountDOM: ({ modal }) => {
+      const focusTrap = createFocusTrap(modal)
+      focusTrap.activate()
+      focusTraps.set(modal, focusTrap)
+    },
+    onBeforeUnmountDOM: ({ modal }) => {
+      const focusTrap = focusTraps.get(modal)
+      if (!focusTrap) return
+      focusTrap.deactivate()
+    },
   }),
 })
